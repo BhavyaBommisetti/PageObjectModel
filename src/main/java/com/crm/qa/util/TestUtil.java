8072b39a9792ea1d5358e032f48734b0cac6bc6a
BhavyaBommisetti <37726725+BhavyaBommisetti@users.noreply.github.com>
2019-12-02 17:12:51 +0530

    Update TestUtil.java

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -22,7 +22,7 @@ public class TestUtil extends TestBase {
 	public static long IMPLICIT_WAIT = 20;
 
 	public static String TESTDATA_SHEET_PATH = "/var/lib/jenkins/workspace"
-			+ "/S_test/src/main/java/com/crm/qa/testdata/FreeCrmTestData.xlsx";
+			+ "/try/src/main/java/com/crm/qa/testdata/FreeCrmTestData.xlsx";
 
 	static Workbook book;
 	static Sheet sheet;
